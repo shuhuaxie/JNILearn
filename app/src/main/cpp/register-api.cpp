768983f2d4f1dee24ffb13c547448cebdc981188
xieshuhua <xieshuhua@163.com>
2018-12-21 15:07:20 +0800

    打印日志

--- app/src/main/cpp/register-api.cpp
+++ app/src/main/cpp/register-api.cpp
@@ -1,31 +1,33 @@
 #include <jni.h>
 #include <cstdio>
+#include <android/log.h>
 
 JavaVM *g_jvm;
 
-jstring getString()
-{
+jstring getString() {
+    va_list list;
     JNIEnv *env = NULL;
+//    va_start(list, 1);
+//    va_end(list);
     g_jvm->AttachCurrentThread(&env, NULL); //g_jvm为JavaVM指针
-
+    __android_log_vprint(2, "test _ log", "say something", list);
     return env->NewStringUTF("This string is from method by register!");
 }
 
 static JNINativeMethod methods[] = {
-        {"methodByRegister", "()Ljava/lang/String;", reinterpret_cast<void*>(getString)}
+        {"methodByRegister", "()Ljava/lang/String;", reinterpret_cast<void *>(getString)}
 };
 
 
-JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *reserved)
-{
-    JNIEnv* env = NULL;
+JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *reserved) {
+    JNIEnv *env = NULL;
     g_jvm = vm;
-    if (JNI_OK != vm->GetEnv(reinterpret_cast<void**> (&env),JNI_VERSION_1_4)) {
+    if (JNI_OK != vm->GetEnv(reinterpret_cast<void **> (&env), JNI_VERSION_1_4)) {
 //        LOGW("JNI_OnLoad could not get JNI env");
         return JNI_ERR;
     }
     jclass clazz = env->FindClass("jeep/jniLearn/JNIMethods");
 
-    env->RegisterNatives(clazz, methods, sizeof(methods)/sizeof((methods)[0]) );
+    env->RegisterNatives(clazz, methods, sizeof(methods) / sizeof((methods)[0]));
     return JNI_VERSION_1_4;
 }
